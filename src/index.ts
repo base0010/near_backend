958bc6f1a2928ceeb3a5c92899adfc019de0ce4d
Marcus H <marcus@gatekeeper.network>
2020-01-28 12:15:45 -0500

    Working on Main RPC Calls

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,33 @@ import { Blocks,Transactions } from "./database/entity/models"
 import {BlockRepository} from "./database/entity/BlockRepository";
 import {NestFactory} from "@nestjs/core";
 // import {WsAdapter} from '@nestjs/platform-ws'
+import * as allSettled from 'promise.allsettled'
 import {IoAdapter} from "@nestjs/platform-socket.io";
 import {BlocksModule} from "./api/blocks/blocks.module";
-
 import {NearRpc} from './nearRpc'
 
+
+class Result{
+    val:any;
+    err:any;
+    constructor() {
+        this.val = undefined
+        this.err = undefined
+    }
+}
+
+function promiseResult(promise:Promise<any>){
+    return new Promise(res=>{
+        const payload = new Result();
+        promise.then((r)=>{
+            payload.val = r
+        }).catch((e)=>{
+            payload.err = e
+        }).then(()=>{
+            res(payload)
+        })
+    })
+}
 async function bootstrapServer() {
     const app = await NestFactory.create(BlocksModule)
     app.useWebSocketAdapter(new IoAdapter(app))
@@ -26,13 +48,15 @@ function getBlockRepository(connection:any): BlockRepository{
 }
 
 class BlockSync {
-    connection!:Connection
-    blockRepository!:BlockRepository
-    provider!:NearRpc
+    connection!: Connection
+    blockRepository!: BlockRepository
+    provider!: NearRpc
 
-    isPolling:boolean = false;
+    isPolling: boolean;
 
     constructor() {
+        this.isPolling = false
+        // this.start().catch(console.error)
     }
 
     async start() {
@@ -51,34 +75,41 @@ class BlockSync {
 
 
     }
-    async getBlocks(){
-        if(this.isPolling){
-            const MAX_PROMISES = 500
 
-
-            let responses = []
-            for(let i = 0; i < MAX_PROMISES; i++){
-                responses.push(this.provider.getBlock(i))
-            }
-            let results = await Promise.all(responses)
-
-            let self = this
-            results.map(async function(res){
-                try {
-                    await self.blockRepository.createAndSave({id: res.header.height, hash: res.header.hash})
-                }catch (e) {
-                    console.error(e)
-                }
-
+    async  saveBlocksFromRequests(requests:any[]) {
+        console.log('insidesave')
+        const responses = await Promise.all(requests.map(([_, req]) => req));
+        const blocks = responses.flatMap((blockResult:any, index) => {
+            const blockHeight = requests[index][0];
+            if(blockResult.value)
+                console.log(blockResult)
+            return [blockResult.value];
         })
     }
+
+    async getBlocks(startingBlock: number, endingBlock: number) {
+        if (this.isPolling) {
+            let height = 10
+            const requests = []
+            const saves = []
+
+            while(height > 0){
+                requests.push([height,promiseResult(this.provider.getBlock(height))])
+                --height;
+
+                saves.push(this.saveBlocksFromRequests(requests))
+
+            }
+
+            await Promise.all(saves)
+        }
     }
 }
 
 const sync = new BlockSync()
 
  sync.start().then(async function(){
-     await sync.getBlocks()
+     await sync.getBlocks(1,100000)
  })
 
 // bootstrapServer().catch(console.error)
